3b2905e754821762f02587baf81911c984368cbf
Ajoy31097 <123059362+Ajoy31097@users.noreply.github.com>
2023-06-12 15:37:55 +0530

    Error condition updated

--- Calculator.js
+++ Calculator.js
@@ -5,21 +5,26 @@ parentElement.addEventListener('click', (e) => {
   const clickedElement = e.target;
 
   if (clickedElement.innerHTML === '=') {
-    calculate = eval(calculate);
-    document.querySelector('#input').value = calculate;
-  } else if(clickedElement.innerHTML == 'C'){
+    try {
+      calculate = eval(calculate);
+      if (isNaN(calculate)) {
+        calculate = "Error";
+      }
+      document.querySelector('#input').value = calculate;
+    } catch (error) {
+      calculate = "Error";
+      document.querySelector('#input').value = calculate;
+    }
+  } else if (clickedElement.innerHTML === 'C') {
     calculate = "";
     document.querySelector('#input').value = calculate;
-  } else if(clickedElement.innerHTML == 'D'){
-    calculate = calculate.slice(0,-1);
+  } else if (clickedElement.innerHTML === 'D') {
+    calculate = calculate.slice(0, -1);
     document.querySelector('#input').value = calculate;
-  
-   } else if(clickedElement.innerHTML === "%"){
-    calculate = eval(calculate)/100;
+  } else if (clickedElement.innerHTML === '%') {
+    calculate = eval(calculate) / 100;
     document.querySelector('#input').value = calculate;
-   
   } else if (clickedElement.innerHTML) {
-    console.log(clickedElement);
     calculate = calculate + clickedElement.innerHTML;
     document.querySelector('#input').value = calculate;
   }

e5e8ad48dc3273e61d0fb34625cb84a2ba187fc7
Ajoy31097 <123059362+Ajoy31097@users.noreply.github.com>
2023-06-12 14:31:22 +0530

    Updated Calc

--- Calculator.js
+++ Calculator.js
@@ -7,16 +7,18 @@ parentElement.addEventListener('click', (e) => {
   if (clickedElement.innerHTML === '=') {
     calculate = eval(calculate);
     document.querySelector('#input').value = calculate;
-  } else if (clickedElement.innerHTML === '%') {
-    const percentage = eval(calculate) / 100;
-    calculate = calculate + '*' + percentage;
-    document.querySelector('#input').value = eval(calculate);
-  } else if(clickedElement.innerHTML == 'AC'){
+  } else if(clickedElement.innerHTML == 'C'){
     calculate = "";
     document.querySelector('#input').value = calculate;
-  }
+  } else if(clickedElement.innerHTML == 'D'){
+    calculate = calculate.slice(0,-1);
+    document.querySelector('#input').value = calculate;
   
-  else if (clickedElement.innerHTML) {
+   } else if(clickedElement.innerHTML === "%"){
+    calculate = eval(calculate)/100;
+    document.querySelector('#input').value = calculate;
+   
+  } else if (clickedElement.innerHTML) {
     console.log(clickedElement);
     calculate = calculate + clickedElement.innerHTML;
     document.querySelector('#input').value = calculate;
